31785fb3f862f9f2c819dc4b27f215f7308a12be
KIMSEONGTAN <kst@KIMSEONGTANui-MacBook-Air.local>
2015-04-25 03:30:57 +0900

    - 이미지 파일 추가 - 스토리 보드 수정 - 날씨 서비스 클래스 추가

---
+++ VWAlarm/WeatherServiece.h
@@ -0,0 +1,13 @@
+//
+//  WeatherServiece.h
+//  VWAlarm
+//
+//  Created by KIMSEONGTAN on 2015. 4. 25..
+//  Copyright (c) 2015년 vwa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WeatherServiece : NSObject
+
+@end
---
+++ VWAlarm/WeatherServiece.m
@@ -0,0 +1,13 @@
+//
+//  WeatherServiece.m
+//  VWAlarm
+//
+//  Created by KIMSEONGTAN on 2015. 4. 25..
+//  Copyright (c) 2015년 vwa. All rights reserved.
+//
+
+#import "WeatherServiece.h"
+
+@implementation WeatherServiece
+
+@end
